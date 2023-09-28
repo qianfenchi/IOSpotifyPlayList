f3c8efc4c2c94505b165a214daff32fb30a53f19
qianfenchi <qianfenchi@msn.cn>
2023-09-28 14:16:25 +0000

    update search function

--- IOSpotifyPlayList.py
+++ IOSpotifyPlayList.py
@@ -11,43 +11,38 @@ import re
 import string
 from chinese_converter import to_simplified
 
-artist_map = {}
-with open('artist.csv', encoding='utf8', newline='') as f:
-  reader = csv.reader(f)
-  for row in reader:
-    en, cs = row
-    artist_map[en] = cs
 
-artist_set = set()
+artist_map = {}
+
 
 def search_for_track(sp, track_name, artist_name):
   limit = 10
   offset = 0
   test_artist = None
-  def to_cmp(s):
-    return to_simplified(re.sub('[（）《》·「」%s\s]'%re.escape(string.punctuation), '', s.lower()))
+  def fmt(s):
+    s = to_simplified(s.replace('夢', '梦').lower())
+    return re.sub('[（《「(].*', '', s).strip() if re.sub('[（《「(].*', '', s).strip() else re.sub('[（《「(]', '', s).strip()  
   while True:
-    search_ret = sp.search(track_name, limit=limit, offset=offset)
+    search_ret = sp.search(fmt(track_name), limit=limit, offset=offset)
     search_tracks = search_ret["tracks"]["items"]
     offset += len(search_tracks)
     
     for track in search_tracks:
       test_name  = track["name"]
-      if to_cmp(track_name) != to_cmp(test_name):
-        continue
       test_artist = track["artists"][0]["name"]
-      if test_artist in artist_map:
-        test_artist = artist_map[test_artist]
-      if to_cmp(artist_name) == to_cmp(test_artist):
+      if fmt(test_name) != fmt(track_name): 
+        continue
+      if artist_name == test_artist or (artist_name in artist_map and artist_map[artist_name] == test_artist):
         return True , track["uri"]
+      artist_ret = sp.search(artist_name, type='artist')['artists']['items']
+      for artist in artist_ret:
+        if artist['name'] == test_artist:
+            artist_map[artist_name] = test_artist
+            return True, track['uri']
     if offset > 100 or len(search_tracks) < limit:
         break
 
-  # if offset == 1:
-  #   print(f"return the only one track found: {track_name} - {test_artist}")
-  #   return True, track["uri"]
-  print(f"track not found: {track_name} - {artist_name}")
-  artist_set.add(artist_name)
+  print(f"track not found for: {track_name} - {artist_name}")
   return False, ""
 
 
@@ -68,13 +63,13 @@ def get_playlist(sp, playlist_name):
   offset = 0
   ret = None
   while not ret:
-    play_lists = sp.user_playlists(user_id, limit=limit, offset=offset)["items"]
-    for play_list in play_lists:
-      if play_list["name"].lower() == playlist_name.lower():
-        ret = play_list["uri"]
+    playlists = sp.user_playlists(user_id, limit=limit, offset=offset)["items"]
+    offset += len(playlists)
+    for playlist in playlists:
+      if playlist["name"].lower() == playlist_name.lower():
+        ret = playlist["uri"]
         break
-    offset += len(play_lists)
-    if offset < limit:
+    if len(playlists) < limit:
         break
   return ret
 
@@ -103,7 +98,7 @@ def process(sp, playlist_name, playlist_url, update=False):
     cnt += 1
     if ret and track_uri not in items:
       items.append(track_uri)
-      # print(f"Import to \"{playlist_name}\"({cnt} / {tracks_count}) : " + track["name"] + " - " + track["ar"][0]["name"])
+      print(f"Import to \"{playlist_name}\"({cnt} / {tracks_count}) : " + track["name"] + " - " + track["ar"][0]["name"])
 
   # You can add a maximum of 100 tracks per request
   res = sp.playlist_replace_items(playlist_id, items[:100])
@@ -136,14 +131,13 @@ def main():
   sp = creat_sp()
 
   with open(args.CSVfile, encoding="utf8", newline='') as f:
-    reader = csv.reader(f, delimiter=';')
+    reader = csv.reader((line for line in f if not line.startswith('#')), delimiter=';')
     for row in reader:
       playlist_url, playlist_name, update = row
-      process(sp, playlist_name, playlist_url, update='True') # always update for now
+      process(sp, playlist_name, playlist_url, update=update)
   
 if __name__ == '__main__':
   main()
-  print(artist_set)
 
 
 
